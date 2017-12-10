6072b6465bda9b5ccca5ae24198a0fe04d1f5375
zoff99 <zoff@zoff.cc>
2017-12-10 17:25:51 +0100

    less verbose

--- toxav/bwcontroller.c
+++ toxav/bwcontroller.c
@@ -177,7 +177,7 @@ void bwc_add_lost_v3(BWController *bwc, uint32_t bytes_lost)
     }
 
     if (bytes_lost > 0) {
-        LOGGER_WARNING(bwc->m->log, "BWC lost(1): %d", (int)bytes_lost);
+        LOGGER_DEBUG(bwc->m->log, "BWC lost(1): %d", (int)bytes_lost);
 
         bwc->cycle.lost = bwc->cycle.lost + bytes_lost;
         send_update(bwc);
@@ -191,7 +191,7 @@ void bwc_add_recv(BWController *bwc, uint32_t recv_bytes)
         return;
     }
 
-    LOGGER_WARNING(bwc->m->log, "BWC recv: %d", (int)recv_bytes);
+    // LOGGER_WARNING(bwc->m->log, "BWC recv: %d", (int)recv_bytes);
 
     bwc->cycle.recv = bwc->cycle.recv + recv_bytes;
     send_update(bwc);
@@ -244,7 +244,7 @@ static int on_update(BWController *bwc, const struct BWCMessage *msg)
     uint32_t recv = net_ntohl(msg->recv);
     uint32_t lost = net_ntohl(msg->lost);
 
-    LOGGER_INFO(bwc->m->log, "recved: %u lost: %u", recv, lost);
+    // LOGGER_INFO(bwc->m->log, "recved: %u lost: %u", recv, lost);
 
     if (lost && bwc->mcb) {
 
