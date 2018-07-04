073ae306377f10582859a07b2eedaf86e5fa1f4e
Zoff <zoff99@users.noreply.github.com>
2018-07-04 12:02:54 +0000

    Update b.c

--- toxav/b.c
+++ toxav/b.c
@@ -1,5 +1,10 @@
 /*
- * gcc -O0 -g dummy_ntp.c b.c -o b.bin
+ * gcc -O0 -g -DUNIT_TESTING_ENABLED dummy_ntp.c b.c -o b.bin
+ *
+ * cp /cygdrive/c/Users/$USER/Desktop/b.c .
+ * cp /cygdrive/c/Users/$USER/Desktop/dummy_ntp.c .
+ * cp /cygdrive/c/Users/$USER/Desktop/dummy_ntp.h .
+ * i686-w64-mingw32-gcc -O0 -DUNIT_TESTING_ENABLED -g dummy_ntp.c b.c -o b.bin
  */
 
 #include "dummy_ntp.h"
