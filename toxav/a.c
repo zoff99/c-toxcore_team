5e816f19909cc4022540c0ba3e8e0b075f329718
Zoff <zoff99@users.noreply.github.com>
2018-07-04 12:02:32 +0000

    Update a.c

--- toxav/a.c
+++ toxav/a.c
@@ -1,5 +1,10 @@
 /*
- * gcc -O0 -g ts_buffer.c a.c -o a.bin
+ * gcc -O0 -g -DUNIT_TESTING_ENABLED ts_buffer.c a.c -o a.bin
+ *
+ * cp /cygdrive/c/Users/$USER/Desktop/a.c .
+ * cp /cygdrive/c/Users/$USER/Desktop/ts_buffer.c .
+ * cp /cygdrive/c/Users/$USER/Desktop/ts_buffer.h .
+ * i686-w64-mingw32-gcc -O0 -DUNIT_TESTING_ENABLED -g ts_buffer.c a.c -o a.bin
  */
 
 #include "ts_buffer.h"
